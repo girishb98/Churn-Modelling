9d9c7167588e8532fcb51a9686df9789d50f3779
girishb98 <32607580+girishb98@users.noreply.github.com>
2018-05-04 00:36:06 +0530

    Update evaluating_improving_tuning.py

--- evaluating_improving_tuning.py
+++ evaluating_improving_tuning.py
@@ -1,15 +1,4 @@
-# Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
 
 # Importing the libraries
 import numpy as np
